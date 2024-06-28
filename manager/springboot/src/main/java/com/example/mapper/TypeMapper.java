acffc596075a49a22648c47aa31951345dfe1ea4
Fdsay <20214854@cqu.edu.cn>
2024-06-28 10:48:50 +0800

    新增TypeMapper

--- manager/springboot/src/main/java/com/example/mapper/TypeMapper.java
+++ manager/springboot/src/main/java/com/example/mapper/TypeMapper.java
@@ -1,6 +1,6 @@
 package com.example.mapper;
 
-import com.example.entity.Notice;
+import com.example.entity.Type;
 
 import java.util.List;
 
@@ -10,28 +10,28 @@ import java.util.List;
 public interface TypeMapper {
 
     /**
-      * 新增
-    */
-    int insert(Notice notice);
+     * 新增
+     */
+    int insert(Type type);
 
     /**
-      * 删除
-    */
+     * 删除
+     */
     int deleteById(Integer id);
 
     /**
-      * 修改
-    */
-    int updateById(Notice notice);
+     * 修改
+     */
+    int updateById(Type type);
 
     /**
-      * 根据ID查询
-    */
-    Notice selectById(Integer id);
+     * 根据ID查询
+     */
+    Type selectById(Integer id);
 
     /**
-      * 查询所有
-    */
-    List<Notice> selectAll(Notice notice);
+     * 查询所有
+     */
+    List<Type> selectAll(Type type);
 
 }
